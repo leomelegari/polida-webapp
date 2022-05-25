da33f071a5ba9a8c1833b60e8e13a8dc6a0b7f93
Luiz Antonio Motta <mottaluizantonio@gmail.com>
2022-05-25 09:35:43 -0300

    fix: minor fixes in order to run.

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -2,12 +2,12 @@ import { useState } from "react";
 import { Redirect } from "react-router-dom";
 import Header from "../../components/Header";
 import { Container } from "./styles";
-import Client from "../client";
+import Client from "../Client";
 import Users from "../users";
 
 const Dashboard = ({ auth }) => {
   const [actualPage, setActualPage] = useState("dashboard");
- console.log("actualPage ", actualPage);
+  console.log("actualPage ", actualPage);
 
   if (!auth) {
     return <Redirect to="/login" />;
