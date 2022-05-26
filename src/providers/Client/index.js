bbf216141254c3613bb5810666816034f75b8505
Igor Sousa <igodrisousa@gmail.com>
2022-05-26 09:58:45 -0300

    fix: correct function

--- src/providers/Client/index.js
+++ src/providers/Client/index.js
@@ -2,41 +2,46 @@ import { createContext, useContext, useState } from "react";
 import api from "../../services/api";
 
 const ClientsContext = createContext({});
-const UserContext = createContext({});
+// const UserContext = createContext({});
 const initial = [];
 
 export const ClientsProvider = ({ children }) => {
-    const [clients, setClients] = useState(initial);
-    const [users, setUsers] = useState(initial);
+  const [clients, setClients] = useState(initial);
+  const [setUsers] = useState(initial);
 
-    const token = JSON.parse(localStorage.getItem("@POLIDA:token")) || [];
+  const token = JSON.parse(localStorage.getItem("@POLIDA:token")) || [];
 
-    const listClients = async () => {
-        await api.get(`/clients`, {
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
-        }).then((res) => {
-            setClients(res.data)
+  const listClients = async () => {
+    await api
+      .get(`/clients`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((res) => {
+        setClients(res.data);
+      });
+  };
 
-        })
-    }
+  const listUsers = async () => {
+    await api
+      .get("/users", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((res) => {
+        setUsers(res.data);
+      });
+  };
 
-    const listUsers = async () => {
-        await api.get("/users", {
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
-        }).then((res) => {
-            setUsers(res.data)
-        })
-    }
-
-    return (
-        <ClientsContext.Provider value={{ clients, setClients, listClients, listUsers }}>
-            {children}
-        </ClientsContext.Provider>
-    )
-}
+  return (
+    <ClientsContext.Provider
+      value={{ clients, setClients, listClients, listUsers }}
+    >
+      {children}
+    </ClientsContext.Provider>
+  );
+};
 
 export const useClients = () => useContext(ClientsContext);
